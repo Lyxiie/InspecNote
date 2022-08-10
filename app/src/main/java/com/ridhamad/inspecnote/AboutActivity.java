b58ab3c74ad42a55d12c8467cebea272285abaf1
Lyxiie <99527267+Lyxiie@users.noreply.github.com>
2022-08-10 22:55:07 +0700

    Add Privacy Policy, About

---
+++ app/src/main/java/com/ridhamad/inspecnote/AboutActivity.java
@@ -0,0 +1,31 @@
+package com.ridhamad.inspecnote;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ActionBar;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("About");
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
